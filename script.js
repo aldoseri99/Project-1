dd448e6a5b7f9255e3eb9a09ca55d035a44e2f93
aldoseri99 <aldosariali9@gmail.com>
2024-09-06 17:08:37 +0300

    move to up func.

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ let direction = ''
 let startPosition = [7, 5]
 let counter = 1
 let sandwormLenght = 4
+let rightInterval
+let upInterval
 
 const main = document.querySelector('main')
 for (let i = 0; i < 15; i++) {
@@ -26,49 +28,77 @@ for (let i = 0; i < 15; i++) {
 
 const columns = document.querySelectorAll('section')
 
+const sandwormEnds = () => {
+  let shift = sandworm.shift()
+  const cellShift = document.querySelectorAll(`#c${shift[0]} div`)
+  if ((shift[1] + shift[0]) % 2 === 0) {
+    cellShift[shift[1]].style.backgroundColor = '#ff3333'
+  } else {
+    cellShift[shift[1]].style.backgroundColor = '#ff9999'
+  }
+}
+
 const moveRight = () => {
   sandworm.push([...startPosition])
   const cell = document.querySelectorAll(`#c${startPosition[0]} div`)
   cell[startPosition[1]].style.backgroundColor = 'yellow'
   startPosition[0]++
   if (counter >= sandwormLenght) {
-    let shift = sandworm.shift()
-    const cellShift = document.querySelectorAll(`#c${shift[0]} div`)
-    if ((shift[1] + shift[0]) % 2 === 0) {
-      cellShift[shift[1]].style.backgroundColor = '#ff3333'
-    } else {
-      cellShift[shift[1]].style.backgroundColor = '#ff9999'
-    }
+    sandwormEnds()
   }
   counter++
-  for (let i = 0; i < sandworm.length; i++) {
-    console.log(sandworm[i])
+  if (startPosition[0] + 1 === 16) {
+    clearInterval(rightInterval)
+    // console.log('right edge')
   }
-  console.log('-------------------------------')
+  // for (let i = 0; i < sandworm.length; i++) {
+  //   console.log(sandworm[i])
+  // }
+  // console.log('-------------------------------')
+}
+const moveUp = () => {
+  sandworm.push([...startPosition])
+  const cell = document.querySelectorAll(`#c${startPosition[0]} div`)
+  cell[startPosition[1]].style.backgroundColor = 'yellow'
+  startPosition[1]--
+  if (counter >= sandwormLenght) {
+    sandwormEnds()
+  }
+  counter++
+  if (startPosition[1] - 1 === -2) {
+    clearInterval(upInterval)
+    // console.log('up edge')
+  }
+  // for (let i = 0; i < sandworm.length; i++) {
+  //   console.log(sandworm[i])
+  // }
+  // console.log('-------------------------------')
 }
 
 window.addEventListener('keydown', (arrow) => {
   if (
     arrow.key === 'ArrowRight' &&
-    (direction === '' || (direction != 'left' && direction != 'right'))
+    (direction === '' || (direction != 'right' && direction != 'left'))
   ) {
     direction = 'right'
-    setInterval(moveRight, 2000)
+    clearInterval(upInterval)
+    rightInterval = setInterval(moveRight, 100)
   } else if (
     arrow.key === 'ArrowLeft' &&
-    (direction === '' || direction != 'right')
+    (direction === '' || (direction != 'left' && direction != 'right'))
   ) {
     direction = 'left'
     console.log('left')
   } else if (
     arrow.key === 'ArrowUp' &&
-    (direction === '' || direction != 'down')
+    (direction === '' || (direction != 'up' && direction != 'down'))
   ) {
     direction = 'up'
-    console.log('up')
+    clearInterval(rightInterval)
+    upInterval = setInterval(moveUp, 100)
   } else if (
     arrow.key === 'ArrowDown' &&
-    (direction === '' || direction != 'up')
+    (direction === '' || (direction != 'down' && direction != 'up'))
   ) {
     direction = 'down'
     console.log('down')
