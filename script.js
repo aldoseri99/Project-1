102e89a5559244d07b43cf3387b36c54ba4aae9c
aldoseri99 <aldosariali9@gmail.com>
2024-09-08 19:02:18 +0300

    add delay movement

--- script.js
+++ script.js
@@ -4,19 +4,24 @@ let startPosition = [7, 5] //middle of the grid
 let counter = 1
 let sandwormLenght = 4
 let sandwormSpeed = 100
-let rightInterval
-let upInterval
-let leftInterval
-let downInterval
 let moving = true
 let apple = []
 let appleExist = false //  indicator to see if there is an apple in the grid
 let shift //the shifted value
 let head
 let score = 0
+let appleShape //apple icon
+
+//intervals
+let rightInterval
+let upInterval
+let leftInterval
+let downInterval
+let timeInterval
 
 const scoreLabel = document.createElement('h1')
 document.body.append(scoreLabel)
+scoreLabel.innerText = '00'
 const main = document.querySelector('main')
 for (let i = 0; i < 15; i++) {
   let section = document.createElement('section')
@@ -28,7 +33,7 @@ for (let i = 0; i < 15; i++) {
     div.style.width = '50px'
     div.style.height = '50px'
     div.style.border = '1px solid black'
-    div.innerText = `${i},${j}`
+    // div.innerText = `${i},${j}`
     if ((j + i) % 2 === 0) {
       div.style.backgroundColor = '#ff3333'
     } else {
@@ -41,22 +46,18 @@ for (let i = 0; i < 15; i++) {
 
 const sandwormMove = () => {
   sandworm.push([...startPosition]) //from geek
-  const cell = document.querySelectorAll(`#c${startPosition[0]} div`)
-  // for (let i = 0; i < sandworm.length; i++) {
-
-  // const cell = document.querySelector(`#c${startPosition[0]} :nth-child(${startPosition[1]+1})`)
-  // if (
-  //   startPosition[0] === sandworm[sandworm.length - 1][0] &&
-  //   startPosition[1] === sandworm[sandworm.length - 1][1]
-  // ) {
-  //   console.log(
-  //     `${startPosition[0]} === ${sandworm[sandworm.length - 1][0]}
-  //       ${startPosition[1]} === ${sandworm[sandworm.length - 1][1]}`
-  //   )
-  //   cell[startPosition[1]].style.backgroundColor = 'blue'
-  // }
-  // }
-  cell[startPosition[1]].style.backgroundColor = 'yellow'
+  const cell = document.querySelector(
+    `#c${startPosition[0]} div:nth-child(${startPosition[1] + 1})`
+  )
+  cell.style.backgroundColor = 'blue'
+  for (let i = 0; i < sandworm.length - 1; i++) {
+    let c = sandworm[i][0]
+    let r = sandworm[i][1]
+    const cell = document.querySelector(
+      `#c${sandworm[i][0]} div:nth-child(${sandworm[i][1] + 1})`
+    )
+    cell.style.backgroundColor = 'green'
+  }
 }
 
 const sandwormEnds = () => {
@@ -78,10 +79,10 @@ const addApple = () => {
     apple = [c, r]
     appleExist = true
   } while (sandworm.find(isThere))
-  let appleShape = document.querySelector(
+  appleShape = document.querySelector(
     `#c${apple[0]} :nth-child(${apple[1] + 1})`
   )
-  appleShape.style.backgroundColor = 'green'
+  appleShape.innerHTML = '<i class="fa-solid fa-apple-whole"></i>'
 }
 
 const isThere = (position) => {
@@ -93,7 +94,6 @@ const isThere = (position) => {
 
 const checkEatSelf = () => {
   let mouth = sandworm[sandworm.length - 1]
-  console.log(mouth)
   for (let i = 0; i < sandworm.length - 1; i++) {
     if (mouth[0] === sandworm[i][0] && mouth[1] === sandworm[i][1]) {
       moving = false
@@ -102,28 +102,39 @@ const checkEatSelf = () => {
       clearInterval(rightInterval)
       clearInterval(leftInterval)
       console.log('just eat it')
+      break
     }
   }
+}
+
+const appleEaten = () => {
+  if (
+    sandworm[sandworm.length - 1][0] === apple[0] &&
+    sandworm[sandworm.length - 1][1] === apple[1]
+  ) {
+    appleShape.innerHTML = ''
+    score += 10
+    scoreLabel.innerText = score
+    addApple()
+  } else if (counter >= sandwormLenght) {
+    sandwormEnds()
+  }
+}
+
+const timer = () => {
+  score += 10
+  scoreLabel.innerText = score
 }
 
 const moveRight = () => {
   if (startPosition[0] + 1 < 15) {
     startPosition[0]++
-    sandwormMove()
     checkEatSelf()
+    sandwormMove()
     if (appleExist === false) {
       addApple()
     }
-    if (
-      sandworm[sandworm.length - 1][0] === apple[0] &&
-      sandworm[sandworm.length - 1][1] === apple[1]
-    ) {
-      score += 10
-      scoreLabel.innerText = score
-      addApple()
-    } else if (counter >= sandwormLenght) {
-      sandwormEnds()
-    }
+    appleEaten()
     counter++
   } else {
     moving = false
@@ -138,16 +149,7 @@ const moveUp = () => {
     if (appleExist === false) {
       addApple()
     }
-    if (
-      sandworm[sandworm.length - 1][0] === apple[0] &&
-      sandworm[sandworm.length - 1][1] === apple[1]
-    ) {
-      score += 10
-      scoreLabel.innerText = score
-      addApple()
-    } else if (counter >= sandwormLenght) {
-      sandwormEnds()
-    }
+    appleEaten()
     counter++
   } else {
     moving = false
@@ -163,16 +165,7 @@ const moveLeft = () => {
     if (appleExist === false) {
       addApple()
     }
-    if (
-      sandworm[sandworm.length - 1][0] === apple[0] &&
-      sandworm[sandworm.length - 1][1] === apple[1]
-    ) {
-      score += 10
-      scoreLabel.innerText = score
-      addApple()
-    } else if (counter >= sandwormLenght) {
-      sandwormEnds()
-    }
+    appleEaten()
     counter++
   } else {
     moving = false
@@ -188,16 +181,7 @@ const moveDown = () => {
     if (appleExist === false) {
       addApple()
     }
-    if (
-      sandworm[sandworm.length - 1][0] === apple[0] &&
-      sandworm[sandworm.length - 1][1] === apple[1]
-    ) {
-      score += 10
-      scoreLabel.innerText = score
-      addApple()
-    } else if (counter >= sandwormLenght) {
-      sandwormEnds()
-    }
+    appleEaten()
     counter++
   } else {
     moving = false
@@ -206,11 +190,22 @@ const moveDown = () => {
 }
 
 //source: geeksforgeeks
+let startTime = true
+let keyPress = 0
+let cooldown = 100
+
 window.addEventListener('keydown', (arrow) => {
+  const currentTime = Date.now()
+
+  if (currentTime - keyPress < cooldown) {
+    return
+  }
+  lastKeyPressTime = currentTime
   if (moving) {
     if (
       arrow.key === 'ArrowRight' &&
-      (direction === '' || (direction != 'right' && direction != 'left'))
+      direction != 'right' &&
+      direction != 'left'
     ) {
       direction = 'right'
       clearInterval(upInterval)
@@ -218,7 +213,8 @@ window.addEventListener('keydown', (arrow) => {
       rightInterval = setInterval(moveRight, sandwormSpeed)
     } else if (
       arrow.key === 'ArrowLeft' &&
-      (direction === '' || (direction != 'left' && direction != 'right'))
+      direction != 'left' &&
+      direction != 'right'
     ) {
       direction = 'left'
       clearInterval(upInterval)
@@ -226,7 +222,8 @@ window.addEventListener('keydown', (arrow) => {
       leftInterval = setInterval(moveLeft, sandwormSpeed)
     } else if (
       arrow.key === 'ArrowUp' &&
-      (direction === '' || (direction != 'up' && direction != 'down'))
+      direction != 'up' &&
+      direction != 'down'
     ) {
       direction = 'up'
       clearInterval(rightInterval)
@@ -234,7 +231,8 @@ window.addEventListener('keydown', (arrow) => {
       upInterval = setInterval(moveUp, sandwormSpeed)
     } else if (
       arrow.key === 'ArrowDown' &&
-      (direction === '' || (direction != 'down' && direction != 'up'))
+      direction != 'down' &&
+      direction != 'up'
     ) {
       direction = 'down'
       clearInterval(rightInterval)
